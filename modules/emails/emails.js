d9dd178525fe26a28039c40df324f8f9b53d861f
matthieu Fourtina <mfourtina@gmail.com>
2015-04-18 14:21:35 +0200

    feat(module): attach listener only if the plugin is correctly init

--- modules/emails/emails.js
+++ modules/emails/emails.js
@@ -16,50 +16,52 @@ var imap = new Imap({
  **********************/
 exports.module = function (zeno) {
 
-  /*
-   * @param data.device : device to render
-   * @param data.env    : environment name
-   */
-  zeno.on('onEnvUpdate', function (data) {
-    if (zeno.pages.emails) {
-        console.log('Update Emails');
-        zeno.pages.emails.forEach(function (email) {
-            renderEmail(email.email, email.name);
+    if (imap.user.length && imap.password.length && imap.host.length) {
+        /*
+        * @param data.device : device to render
+        * @param data.env    : environment name
+        */
+        zeno.on('onEnvUpdate', function (data) {
+            if (zeno.pages.emails) {
+                console.log('Update Emails');
+                zeno.pages.emails.forEach(function (email) {
+                    renderEmail(email.email, email.name);
+                });
+            }
+        });
+
+        zeno.io.sockets.on('connection', function (socket) {
+            /*
+             * Fired when an email is refreshed by a client
+             * @param data.name : name to search in mail box
+             * @param data.path : path to write the image
+             */
+            socket.on('refreshOneEmail', function (data) {
+                renderEmail(data.name, data.path);
+            });
         });
     }
-  });
 
-  zeno.io.sockets.on('connection', function (socket) {
-    /*
-     * Fired when an email is refreshed by a client
-     * @param data.name : name to search in mail box
-     * @param data.path : path to write the image
-     */
-    socket.on('refreshOneEmail', function (data) {
-        renderEmail(data.name, data.path);
-    });
-  });
+    function renderEmail(name, path) {
+        var options = {};
+        readEmail(name, function(body) {
+            options = {
+                viewportSize: {width: 1600, height: 1100},
+                body        : body
+            };
 
-  function renderEmail(name, path) {
-    var options = {};
-    readEmail(name, function(body) {
-      options = {
-            viewportSize: {width: 1600, height: 1100},
-            body        : body
-      };
-
-      zeno.emit('takeScreenshot', {
-            url: undefined,
-            path: path,
-            options: options
-      });
-    });
-  };
+            zeno.emit('takeScreenshot', {
+                url: undefined,
+                path: path,
+                options: options
+            });
+        });
+    };
 
   function readEmail(search, cb) {
-      var buffer = '';
+        var buffer = '';
 
-      imap.once('ready', function() {
+        imap.once('ready', function() {
             openInbox(function(err, box) {
                 if (err) throw err;
 
@@ -85,7 +87,7 @@ exports.module = function (zeno) {
                 f.once('end', function() {
                     imap.end();
                 });
-          });
+        });
     });
 
     imap.once('error', function(err) {
